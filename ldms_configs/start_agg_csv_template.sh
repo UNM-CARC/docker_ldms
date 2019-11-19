f7e9c47222fff4d69385df1112186ee12acd6fd1
qwofford <qwofford@datamunger.ddns.net>
2019-11-19 16:30:24 -0700

    make aggregator usable in a container

--- ldms_configs/start_agg_csv_template.sh
+++ ldms_configs/start_agg_csv_template.sh
@@ -1,10 +1,6 @@
 #!/bin/bash -l
-export MODULEPATH=/wheeler/scratch/qwofford/modulefiles
-module load ovis/4.2.3
 
 USER=$(whoami)
-cat /wheeler/scratch/qwofford/opt/ovis/4.2.3/configs/aggregator_csv.conf | sed "s/<USERNAME>/${USER}/" > /dev/shm/agg_${USER}.conf
-cp /wheeler/scratch/qwofford/opt/ovis/4.2.3/configs/ldmsauth_group_readable.conf /wheeler/scratch/${USER}/ldmsauth.conf
-chmod go-r /wheeler/scratch/${USER}/ldmsauth.conf
-LDMS_AUTH_FILE=/wheeler/scratch/${USER}/ldmsauth.conf
-ldmsd -a ovis -A conf=${LDMS_AUTH_FILE} -x sock:10002 -l aggd_csv_${USER}.log -s aggd_csv_${USER}.sock -r aggd_csv_${USER}.pid -c /dev/shm/agg_${USER}.conf -v DEBUG
+cat /wheeler/scratch/qwofford/opt/ovis/4.2.3/configs/aggregator_csv.conf | sed "s/<USERNAME>/${USER}/" > /dev/shm/agg_${USER}.conf;
+export LDMS_AUTH_FILE=/opt/ldms_wheeler/ldmsauth.conf;
+ldmsd -a ovis -A conf=${LDMS_AUTH_FILE} -x sock:10002 -l aggd_csv_${USER}.log -s aggd_csv_${USER}.sock -r aggd_csv_${USER}.pid -c /dev/shm/agg_${USER}.conf -v DEBUG;
