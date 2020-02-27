07c49a1efe5e58a147162dd85f59926e0d4e99be
Quincy Wofford <quincy.wofford@gmail.com>
2020-02-27 12:28:41 -0700

    making port pseudo-random based on UID modulus

--- ldms_configs/start_agg_csv_template.sh
+++ ldms_configs/start_agg_csv_template.sh
@@ -5,4 +5,4 @@ source /home/docker/env.sh
 USER=$(whoami)
 cat /opt/ldms_wheeler/aggregator_csv.conf | sed "s/<USERNAME>/${USER}/" > /dev/shm/agg_${USER}.conf;
 export LDMS_AUTH_FILE=/opt/ldms_wheeler/ldmsauth.conf;
-ldmsd -a ovis -v INFO -m 10M -A conf=${LDMS_AUTH_FILE} -x sock:10002 -l ${LOG_OUTPUT_DIR}/aggd_csv_${USER}.log -s ${LOG_OUTPUT_DIR}/aggd_csv_${USER}.sock -r ${LOG_OUTPUT_DIR}/aggd_csv_${USER}.pid -c /dev/shm/agg_${USER}.conf;
+ldmsd -a ovis -v INFO -m 10M -A conf=${LDMS_AUTH_FILE} -x sock:${LDMS_PORT} -l ${LOG_OUTPUT_DIR}/aggd_csv_${USER}.log -s ${LOG_OUTPUT_DIR}/aggd_csv_${USER}.sock -r ${LOG_OUTPUT_DIR}/aggd_csv_${USER}.pid -c /dev/shm/agg_${USER}.conf;
--- ldms_configs/start_sampler.sh
+++ ldms_configs/start_sampler.sh
@@ -15,6 +15,6 @@ export LDMS_AUTH_FILE=/opt/ldms_wheeler/ldmsauth.conf
 
 sed "s/<hostname>/$(cat /etc/hostname)/g" < /opt/ldms_wheeler/sampler_template.conf > /dev/shm/sampler.conf
 
-ldmsd -a ovis -A conf=${LDMS_AUTH_FILE} -m 10MB -x sock:10002 -l ${LOG_OUTPUT_DIR}/$(hostname)_sampled_$(whoami).log -s ${LOG_OUTPUT_DIR}/$(hostname)_sampled_$(whoami).sock -r ${LOG_OUTPUT_DIR}/$(hostname)_sampled_$(whoami).pid -c /dev/shm/sampler.conf -v DEBUG
+ldmsd -a ovis -A conf=${LDMS_AUTH_FILE} -m 10MB -x sock:${LDMS_PORT} -l ${LOG_OUTPUT_DIR}/$(hostname)_sampled_$(whoami).log -s ${LOG_OUTPUT_DIR}/$(hostname)_sampled_$(whoami).sock -r ${LOG_OUTPUT_DIR}/$(hostname)_sampled_$(whoami).pid -c /dev/shm/sampler.conf -v DEBUG
 
 echo ENDING: start_sampler.sh
