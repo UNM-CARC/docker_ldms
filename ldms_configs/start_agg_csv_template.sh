da7023ee11747c0f78347da03e304e4b2845a07d
Quincy Wofford <quincy.wofford@gmail.com>
2020-02-27 13:59:08 -0700

    update port for aggregator config

--- ldms_configs/start_agg_csv_template.sh
+++ ldms_configs/start_agg_csv_template.sh
@@ -4,5 +4,6 @@ source /home/docker/env.sh
 
 USER=$(whoami)
 cat /opt/ldms_wheeler/aggregator_csv.conf | sed "s/<USERNAME>/${USER}/" > /dev/shm/agg_${USER}.conf;
+cat /dev/shm/agg_${USER}.conf | sed "s/<LDMS_PORT>/${LDMS_PORT}/" > tmp_agg; cat tmp_agg > /dev/shm/agg_${USER}.conf; rm tmp_agg;
 export LDMS_AUTH_FILE=/opt/ldms_wheeler/ldmsauth.conf;
 ldmsd -a ovis -v INFO -m 10M -A conf=${LDMS_AUTH_FILE} -x sock:${LDMS_PORT} -l ${LOG_OUTPUT_DIR}/aggd_csv_${USER}.log -s ${LOG_OUTPUT_DIR}/aggd_csv_${USER}.sock -r ${LOG_OUTPUT_DIR}/aggd_csv_${USER}.pid -c /dev/shm/agg_${USER}.conf;
